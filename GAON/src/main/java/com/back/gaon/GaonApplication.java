7b873ab9b8f0f304ffd2061c66fc4b6fe24d92a4
junseokPP <pjs981130@gmail.com>
2025-11-02 13:40:06 +0900

    start

---
+++ GAON/src/main/java/com/back/gaon/GaonApplication.java
@@ -0,0 +1,13 @@
+package com.back.gaon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaonApplication.class, args);
+    }
+
+}
---
+++ GAON/src/test/java/com/back/gaon/GaonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.gaon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
